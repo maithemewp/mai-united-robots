9b6e352f062ce14d791edab85b0a44b787148ae0
JiveDig <jivetheuniversal@gmail.com>
2023-12-27 13:43:35 -0500

    Save body as post meta without encoding. Should serialize now.

--- classes/class-listener.php
+++ classes/class-listener.php
@@ -125,7 +125,8 @@ class Mai_United_Robots_Listener {
 		// If not updating an existing post.
 		if ( ! $update ) {
 			// Save the body for reference.
-			update_post_meta( $this->post_id, 'unitedrobots_body', wp_json_encode( $this->body ) );
+			// update_post_meta( $this->post_id, 'unitedrobots_body', wp_json_encode( $this->body ) );
+			update_post_meta( $this->post_id, 'unitedrobots_body', $this->body );
 
 			// This should be overridden in child classes.
 			$this->process();
